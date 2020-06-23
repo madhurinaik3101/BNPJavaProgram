8b0410042b24253ff1eb0b257d90a17d5719ceef
maddynaik <37347515+maddynaik@users.noreply.github.com>
2020-06-23 11:08:50 +0530

    Add files via upload

---
+++ AddNumbersAndStrings.java
@@ -0,0 +1,24 @@
+class AddNumbersAndStrings{
+	int total;
+	void add(int...numbers){
+	for(int number:numbers){
+		total+=number;
+	}
+	System.out.println(total);
+    }
+
+   void add(String...strings){
+	StringBuffer str= new StringBuffer();
+	for(String string:strings){
+		str.append(string);
+	}
+	System.out.println(str);
+    }
+	public static void main(String[] args) {
+		AddNumbersAndStrings n = new AddNumbersAndStrings();
+		n.add(1,2,3,4,5);
+		n.add("Madhuri"," ","Naik");
+
+	}
+}
+
